90ff632a0855aafa4c18b13d6a026bcca7e4633a
rondonerick-source <rondonerick@gmail.com>
2026-08-16 23:38:18 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "medidor-agua-v1";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ARCHIVOS);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
